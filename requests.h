d7f1607b258490718682180b4e77c45939cf4780
Mark Koester <koesterm@umich.edu>
2017-04-15 18:11:42 -0400

    Better create

--- requests.h
+++ requests.h
@@ -19,7 +19,7 @@ char *createResponse(Request *request, unsigned &response_size);
 char *getName(const char *pathname);
 fs_inode *getParentInode(const char *pathname);
 fs_direntry *findEmptyDirentry(fs_inode *inode);
-void addDirentry(fs_inode *dir_inode, const uint32_t dir_inode_block, const char * file_name, const uint32_t file_inode_block);
+bool findDirentry(fs_inode *dir_inode, const char *filename, uint32_t *direntry_block, uint32_t *direntry_idx, fs_direntry *direntries);
 uint32_t removeDirentry(fs_inode *dir_inode, const uint32_t dir_inode_block, const char *filename);
 uint32_t findBlock(fs_inode *parent, const char *name);
 uint32_t traversePath(const Path &path, int depth);
