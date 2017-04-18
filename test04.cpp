44a183ab3cba7484c7a960495cc268de8e5ccffb
apsharon <apsharon@umich.edu>
2017-04-18 15:42:42 -0400

    test cases

--- test04.cpp
+++ test04.cpp
@@ -30,11 +30,45 @@ int main(int argc, char* argv[])
 
     fs_clientinit(server, port);
     fs_session(username, password, &session, seq++);
-    fs_create(username, password, session, seq++, "/andrew", 'f');
-    fs_writeblock(username, password, session, seq++, "/andrew", 0, words);
-    fs_readblock(username, password, session, seq++, "/andrew", 0, buf);
+    fs_create(username, password, session, seq++, "/andrew", 'd');
+    fs_create(username, password, session, seq++, "/andrew/hi", 'd');
+    fs_delete(username, password, session, seq++, "/andrew/hi");
+    fs_create(username, password, session, seq++, "/andrew/hi", 'f');
+    fs_delete(username, password, session, seq++, "/andrew/hi");
+    fs_create(username, password, session, seq++, "/andrew/bye", 'd');
+
+    unsigned int ses = 0;
+    unsigned int seq2= 0;
+    fs_session("user2", "password2", &ses, seq2);
+    fs_writeblock("user2", "password2", ses, seq2++, "/andrew/hi", 0, words);  
+    fs_readblock("user2", "password2", ses, seq2++, "/andrew/hi", 1, buf);
+   
+
+    fs_writeblock(username, password, session, seq++, "/andrew/hi", 0, words);
     
-    fs_delete(username, password, session, seq++, "/andrew");
+    fs_readblock("user2", "password2", ses, seq2++, "/andrew/hi", 0, buf);
+ 
+    fs_create("user2", "password2", ses, seq2++, "/andrew/bye/hello", 'f');
+    fs_create("user2", "password2", ses, seq2++, "/andrew/bye/lol", 'd');
+
+    fs_writeblock(username, password, session, seq++, "/andrew/hi", 1, words);
+    fs_readblock(username, password, session, seq++, "/andrew/hi", 0, buf);
+    fs_readblock(username, password, session, seq++, "/andrew/hi", 1, buf);
+    fs_readblock(username, password, session, seq++, "/andrew/hi", 2, buf);
+    fs_writeblock(username, password, session, seq++, "/andrew/hi", 3, words);
     
+    fs_delete("user2", "password2", ses, seq2++, "/andrew/hi");
+
+    fs_delete(username, password, session, seq++, "/andrew/hi");
+    fs_delete(username, password, session, seq++, "/");
+   
+    unsigned int sess = 0;
+    unsigned int seq3 = 0;
+    fs_session("user3", "wrong", &sess, seq3++);
+    sess = 0;
+    fs_session("nope", "password3", &sess, seq3++);     
+    sess = 1;
+    fs_session("user3", "password3", &sess, seq3++); 
+
     return 0;
 }
