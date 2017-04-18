ddf576d4c8faf1c6029c959f7747eeaef67b1ff4
Mark Koester <koesterm@umich.edu>
2017-04-18 11:40:48 -0400

    Fixed deadlock

--- LockManager.cpp
+++ LockManager.cpp
@@ -4,33 +4,41 @@ using namespace std;
 void LockManager::aquireReadLock(uint32_t block) {
     mapLock.lock();
 
-    lockMap[block].readerStart();
+    RWLock *rwlock = &lockMap[block];
 
     mapLock.unlock();
+
+    rwlock->readerStart();
 }
 
 void LockManager::releaseReadLock(uint32_t block) {
     mapLock.lock();
 
-    lockMap[block].readerFinish();
+    RWLock *rwlock = &lockMap[block];
 
     mapLock.unlock();
+
+    rwlock->readerFinish();
 }
 
 void LockManager::aquireWriteLock(uint32_t block) {
     mapLock.lock();
 
-    lockMap[block].writerStart();
+    RWLock *rwlock = &lockMap[block];
 
     mapLock.unlock();
+
+    rwlock->writerStart();
 }
 
 void LockManager::releaseWriteLock(uint32_t block) {
     mapLock.lock();
 
-    lockMap[block].writerFinish();
+    RWLock *rwlock = &lockMap[block];
 
     mapLock.unlock();
+
+    rwlock->writerFinish();
 }
 
 void LockManager::createLock(uint32_t block) {
